3d5f1e73b5f364d3b5ac35cfc80c72f798aac914
GameplayApps <gameplayapps@gmail.com>
2016-03-13 07:07:47 -0400

    Update SpaceGeekSpeechlet.java

--- samples/src/main/java/spacegeek/SpaceGeekSpeechlet.java
+++ samples/src/main/java/spacegeek/SpaceGeekSpeechlet.java
@@ -7,7 +7,7 @@
 
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-package spacegeek;
+package datehacker;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,30 +29,14 @@ import com.amazon.speech.ui.SimpleCard;
  * This simple sample has no external dependencies or session management, and shows the most basic
  * example of how to handle Alexa Skill requests.
  */
-public class SpaceGeekSpeechlet implements Speechlet {
-    private static final Logger log = LoggerFactory.getLogger(SpaceGeekSpeechlet.class);
+public class DateHackerSpeechlet implements Speechlet {
+    private static final Logger log = LoggerFactory.getLogger(DateHackerSpeechlet.class);
 
     /**
      * Array containing space facts.
      */
-    private static final String[] SPACE_FACTS = new String[] {
-            "A year on Mercury is just 88 days long.",
-            "Despite being farther from the Sun, Venus experiences higher temperatures "
-                    + "than Mercury.",
-            "Venus rotates counter-clockwise, possibly because of a collision in the "
-                    + "past with an asteroid.",
-            "On Mars, the Sun appears about half the size as it does on Earth.",
-            "Earth is the only planet not named after a god.",
-            "Jupiter has the shortest day of all the planets.",
-            "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 "
-                    + "billion years.",
-            "The Sun contains 99.86% of the mass in the Solar System.",
-            "The Sun is an almost perfect sphere.",
-            "A total solar eclipse can happen once every 1 to 2 years. This makes them "
-                    + "a rare event.",
-            "Saturn radiates two and a half times more energy into space than it "
-                    + "receives from the sun.",
-            "The temperature inside the Sun can reach 15 million degrees Celsius.",
+    private static final String[] DATE_FACTS = new String[] {
+            "Based on your location, the best restaurants nearby for a date in downtown Toronto, from least expensive to most expensive, are Le Neuf, Bar Buca, Tutti Matti, Figo, and Alo.",
             "The Moon is moving approximately 3.8 cm away from our planet every year."
     };
 
@@ -115,15 +99,15 @@ public class SpaceGeekSpeechlet implements Speechlet {
      */
     private SpeechletResponse getNewFactResponse() {
         // Get a random space fact from the space facts list
-        int factIndex = (int) Math.floor(Math.random() * SPACE_FACTS.length);
-        String fact = SPACE_FACTS[factIndex];
+        int factIndex = (int) Math.floor(Math.random() * DATE_FACTS.length);
+        String fact = DATE_FACTS[factIndex];
 
         // Create speech output
-        String speechText = "Here's your space fact: " + fact;
+        String speechText = "Here's your dating advice: " + fact;
 
         // Create the Simple card content.
         SimpleCard card = new SimpleCard();
-        card.setTitle("SpaceGeek");
+        card.setTitle("DateHacker");
         card.setContent(speechText);
 
         // Create the plain text output.
@@ -138,7 +122,7 @@ public class SpaceGeekSpeechlet implements Speechlet {
      */
     private SpeechletResponse getHelpResponse() {
         String speechText =
-                "You can ask Space Geek tell me a space fact, or, you can say exit. What can I "
+                "You can ask Date Hacker what are the best restaurants in downtown Toronto for a date, or you can say exit. What can I "
                         + "help you with?";
 
         // Create the plain text output.
